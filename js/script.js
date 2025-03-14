d84a30b9d5877646a43953e61b61b7c3fea67cc7
Łukasz Bilski <bukaszlilski@gmail.com>
2025-03-14 17:22:38 +0100

    to be fixed

---
+++ js/script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // init
+  var controller = new ScrollMagic.Controller();
+
+  // define movement of panels
+  var wipeAnimation = gsap
+    .timeline()
+    // animate to second panel
+    .to("#slideContainer", { duration: 0.5, z: -150 }) // move back in 3D space
+    .to("#slideContainer", { duration: 1, x: "-25%" }) // move in to first panel
+    .to("#slideContainer", { duration: 0.5, z: 0 }) // move back to origin in 3D space
+    // animate to third panel
+    .to("#slideContainer", { duration: 0.5, z: -150, delay: 1 })
+    .to("#slideContainer", { duration: 1, x: "-50%" })
+    .to("#slideContainer", { duration: 0.5, z: 0 })
+    // animate to forth panel
+    .to("#slideContainer", { duration: 0.5, z: -150, delay: 1 })
+    .to("#slideContainer", { duration: 1, x: "-75%" })
+    .to("#slideContainer", { duration: 0.5, z: 0 });
+
+  // create scene to pin and link animation
+  new ScrollMagic.Scene({
+    triggerElement: "#pinContainer",
+    triggerHook: "onLeave",
+    duration: "500%",
+  })
+    .setPin("#pinContainer")
+    .setTween(wipeAnimation)
+    .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+});
